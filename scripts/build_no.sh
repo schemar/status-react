a40bc3fbab054005d65227177c5ca57eeb88617e
Jakub Sokołowski <jakub@status.im>
2018-11-02 13:16:29 +0100

    stop using build-* git tags for counting build numbers
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- scripts/build_no.sh
+++ scripts/build_no.sh
@@ -1,80 +1,43 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -e
 
+#####################################################################
 #
 # This script manages app build numbers.
 # It returns the next build number to be used.
-# If ran with --tag it will mark current HEAD with new build number.
+# The limit of size of the number is signed int, which is 2147483647.
 #
 # These numbers are used to mark app artifacts for:
 # * Play Store - versionCode attribute (gradle)
 # * Apple Store - CFBundleVersion attribute (plutil)
 #
-# The numbers need to be incremeneted and are maintained via
-# git tags matching the '^build-[0-9]+$' regex.
-# Builds from an already tagged commit should use the same number.
-#
 # For more details see:
 # * https://developer.android.com/studio/publish/versioning
 # * https://developer.apple.com/library/content/documentation/General/Reference/InfoPlistKeyReference/Articles/CoreFoundationKeys.html
 #
+# History: This script used to tag builds with `build-[0-9]+` tags.
+#
+#####################################################################
 
-REGEX='^build-[0-9]\+$'
+REPO_ROOT=$(git rev-parse --show-toplevel)
+VERSION_REGEX='([0-9]+).([0-9]+).([0-9]+)'
 
-getNumber () {
-    echo "$1" | sed 's/[^0-9]*//g'
-}
-
-findNumber () (
-    # check if current commit has a build tag
-    # since we are building in separate jobs we have to check for a tag
-    BUILD_TAG=$(git tag --points-at HEAD | grep -e "$REGEX" | tail -n1)
-    
-    # use already existing build number if applicable
-    if [ -n "$BUILD_TAG" ]; then
-        echo "Current commit already tagged: $BUILD_TAG" >&2
-        getNumber $BUILD_TAG
-    fi
-)
-
-tagBuild () {
-    echo "Tagging HEAD: build-$1" >&2
-    git tag "build-$1" HEAD
-    if [ -n "$GIT_USER" ] && [ -n "$GIT_PASS" ]; then
-        git push --tags \
-          https://${GIT_USER}:${GIT_PASS}@github.com/status-im/status-react
+getNumber() {
+    YEAR_AND_DAYS=$(date '+%y%m') # Year + Month
+    VERSION=$(cat ${REPO_ROOT}/mobile_files/VERSION)
+    if [[ ${VERSION} =~ ${VERSION_REGEX} ]]; then
+        MAJOR_VER=$(printf "%02d" "${BASH_REMATCH[1]}")
+        MINOR_VER=$(printf "%02d" "${BASH_REMATCH[2]}")
+        REVIS_VER=$(printf "%03d" "${BASH_REMATCH[3]}")
+        echo "${MAJOR_VER}${MINOR_VER}${REVIS_VER}${YEAR_AND_DAYS}"
     else
-        git push --tags git@github.com:status-im/status-react
+        echo "Unable to parse version number: ${VERSION}"
+        exit 1
     fi
-}
-
-increment () {
-    # find the last used build number
-    BUILD=$(git tag -l --sort=-v:refname | grep -e "$REGEX" | head -n 1)
-    # extract the number
-    BUILD_NO=$(getNumber "$BUILD")
-    
-    # increment
-    BUILD_NO="$((BUILD_NO+1))"
-    # finally print build number
-    echo "$BUILD_NO"
 }
 
 #####################################################################
+# This should result in numbers like: 00090301811 or 01090301811
 
-# make sure we have all the tags
-git fetch --tags --quiet >/dev/null || \
-    >&2 echo "Could not fetch tags from remote"
-
-# check if this commit already has a build number
-NUMBER=$(findNumber)
-
-# if it doesn't, or we are forcing via cli option, increment
-if [ -z "$NUMBER" ] || [ "$1" = "--increment" ]; then
-    NUMBER=$(increment)
-    tagBuild $NUMBER
-fi
-
-# print build number
-echo $NUMBER
+getNumber
